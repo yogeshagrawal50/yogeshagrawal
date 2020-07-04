ba6ca6b229e8eef4e1e1b8853b270dab73782343
Yogesh Agrawal <yogeshagrawal50@gmail.com>
2020-07-04 15:19:12 +0530

    deploy

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react'
-const Life = () => {
+const Resume = () => {
   return (
     <div className="resume" style={{ width :'100%' , height:'100%'}}>
     <iframe src="https://docs.google.com/viewer?srcid=1EdTFokmthvTL5Om7z29-MZyvGQp2z4o4&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="60%" height="100%"></iframe>
@@ -7,6 +7,6 @@ const Life = () => {
   )
 }
 
-export default Life
+export default Resume
 // https://drive.google.com/file/d/1EdTFokmthvTL5Om7z29-MZyvGQp2z4o4/view?usp=sharing
 //<iframe src="https://docs.google.com/viewer?srcid=1EdTFokmthvTL5Om7z29-MZyvGQp2z4o4&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="60%" height="100%"></iframe>
