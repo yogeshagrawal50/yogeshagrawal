52f36970fcba22b5c5ecddc2f20831d02bc86b0d
Yogesh Agrawal <yogeshagrawal50@gmail.com>
2020-07-08 22:12:47 +0530

    updated

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,24 +1,16 @@
 import React from 'react';
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import Work from './Work';
 import Header from '../menu/Header';
 import Project from './Project';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import NavBar from '../menu/NavBar'
 
-=======
-import ScrollToTop from '../menu/ScrollToTop'
->>>>>>> parent of 24a7dbc... updated modal issue
-=======
->>>>>>> parent of 0619390... upadated
 
 import Sidebar from '../menu/Sidebar';
 
 const Portfolio = () => {
 	return (
 		<div className='portfolio'>
-		<div className='header-name-light' ><Header /></div>
-
 			<div className='portfolio-header'>Portfolio.</div>
 
 				<div className='portfolio-container'>
@@ -53,7 +45,3 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
-
-{/* <div id="portfolio-footer-container">
-							<ScrollToTop />
-							</div>  */}
