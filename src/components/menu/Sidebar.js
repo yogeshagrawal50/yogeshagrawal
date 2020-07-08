35fef338c81fa67d70bb7b674204796c64de50bd
Yogesh Agrawal <yogeshagrawal50@gmail.com>
2020-07-08 22:15:05 +0530

    updated

--- src/components/menu/Sidebar.js
+++ src/components/menu/Sidebar.js
@@ -7,7 +7,7 @@ const Sidebar = () => {
     <ul className='sidebar-menu'>
       <li><a href='/portfolio/#work'><span className="sidebar-menu-item"><i className="fas fa-briefcase"></i><span className='menu-label'>Work Experience</span></span></a></li>
       <li><a href='/portfolio/#project'><span className="sidebar-menu-item"><i className="fas fa-file-code"></i><span className='menu-label'>Projects</span></span></a></li>
-      <li><a href='/portfolio/#project'><span className="sidebar-menu-item"><i className="fas fa-id-card"></i><span className='menu-label'>Resume</span></span></a></li>
+      <li><a href='/#/resume'><span className="sidebar-menu-item"><i className="fas fa-id-card"></i><span className='menu-label'>Resume</span></span></a></li>
     </ul>
     </div>
     </div>
