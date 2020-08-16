600b904a9d56d205db11c18e9ab4d5792dff0d86
Yogesh Agrawal <yogeshagrawal50@gmail.com>
2020-08-16 17:17:35 +0530

    Updates

---
+++ precache-manifest.d3d3684277a11228252d8df306983de4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace55fadb98485523ff1be02d6cdbdc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12451a7a569493c141aa",
+    "url": "/static/css/main.b7488505.chunk.css"
+  },
+  {
+    "revision": "4a1c74f8495b7cd1113b",
+    "url": "/static/js/2.cf7b6088.chunk.js"
+  },
+  {
+    "revision": "6ebed127b9b87fdbe9dc08046348e0bb",
+    "url": "/static/js/2.cf7b6088.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12451a7a569493c141aa",
+    "url": "/static/js/main.136ed89f.chunk.js"
+  },
+  {
+    "revision": "8257f36d23b6ceda8a43",
+    "url": "/static/js/runtime-main.b3de3902.js"
+  },
+  {
+    "revision": "0bc93e7df98fdffa945e66612462458d",
+    "url": "/static/media/profilepic.0bc93e7d.jpg"
+  }
+]);
